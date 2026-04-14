ccd663c1847ad539d7df3a5127715c74b73faecb
mhilding-jpg <mhilding@alumni.unav.es>
2026-04-14 12:19:51 +0200

    Filtering feature for LocationList

--- WEB-INF/classes/LocationList.java
+++ WEB-INF/classes/LocationList.java
@@ -17,17 +17,29 @@ public class LocationList extends HttpServlet {
         res.setContentType("text/html");
         PrintWriter toClient = res.getWriter();
         String category = req.getParameter("category");
-        toClient.println("<h1 align=\"center\"><a href=\"index.html\">Study Spots Donostia</a></h1>");
+        toClient.println("<h1 align=\"center\"><a href='/StudySpot_Donostia/index'>Study Spots Donostia</a></h1>");
         toClient.println("<table border='1' align='center'>");
         toClient.println("<tr><td>Name</td><td>Rating</td><td>Category</td><td>Location</td><td>Photo</td><td>More Info</td></tr>");
         Vector<LocationData> locationList;
-        if (category != null) {
+        if (category != null && !category.isEmpty()) {
             locationList = LocationData.getLocationList(connection, category);
+            System.out.print(locationList);
         } else {
             locationList = LocationData.getLocationList(connection);
         }
 
         System.out.println(locationList.size());
+        toClient.println("<div id='categoryFilter' align='center'>");
+        toClient.println("<form action='/StudySpot_Donostia/LocationList'>");
+        toClient.println("<select id='category' name='category' size='4' multiple>");
+        toClient.println("<option value='University'>University</option>");
+        toClient.println("<option value='Cafe'>Cafe</option>");
+        toClient.println("<option value='Library'>Library</option>");
+        toClient.println("<option value=''>All</option>");
+        toClient.println("</select>");
+        toClient.println("<input type='submit' value='Submit'>");
+        toClient.println("</form>");
+        toClient.println("</div>");
         for(int i=0; i< locationList.size(); i++){
                 LocationData location = locationList.elementAt(i);
                 toClient.println("<tr>");
