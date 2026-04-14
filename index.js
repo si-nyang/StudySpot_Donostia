d35235f692aaaf869c2c12927e14bec5bf8dae18
si-nyang <144926050+si-nyang@users.noreply.github.com>
2026-04-14 11:33:26 +0200

    please download config.js again

--- index.js
+++ index.js
@@ -2,5 +2,4 @@
 const script = document.createElement("script");
 script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=maps,marker&v=weekly`;
 script.defer = true;
-
 document.head.appendChild(script);
